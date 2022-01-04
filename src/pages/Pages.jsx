68b3c55f0790af1d7ff3b09b7704c6e4fec1ff7b
Victor Pagnier <victor.pagnier@gmail.com>
2022-01-04 19:22:48 +0100

    Burger fini sans le css

--- src/pages/Pages.jsx
+++ src/pages/Pages.jsx
@@ -33,12 +33,7 @@ const Pages = () => {
     );
     return res;
   };
-  return (
-    <div>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-      {createComponent()}
-    </div>
-  );
+  return <div>{createComponent()}</div>;
 };
 
 export default Pages;
