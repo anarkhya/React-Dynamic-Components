244b4100de2a91b82115a37769102bc127093be0
Victor Pagnier <victor.pagnier@gmail.com>
2022-01-04 19:51:08 +0100

    Un peu de css ajouté au menu burger

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -11,7 +11,7 @@ const Header = () => {
           <div className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
             <div className="relative flex items-center justify-between h-16">
               <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
-                <Disclosure.Button className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
+                <Disclosure.Button className="inline-flex items-center justify-center p-2 rounded-md text-teal-900 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
                   <span className="sr-only">Open main menu</span>
                   {open ? (
                     <XIcon className="block h-6 w-6" aria-hidden="true" />
@@ -40,7 +40,7 @@ const Header = () => {
                         <a
                           key={item.name}
                           href={item.to}
-                          className="bg-gray-900 text-white"
+                          className="text-teal-900"
                         >
                           {item.name}
                         </a>
@@ -53,13 +53,10 @@ const Header = () => {
           </div>
 
           <Disclosure.Panel className="sm:hidden">
-            <div className="px-2 pt-2 pb-3 space-y-1">
+            <div className="flex flex-col px-2 pt-2 pb-3 space-y-1">
               {Menu.map((item) => (
                 <Link to={item.to}>
-                  <Disclosure.Button
-                    key={item.name}
-                    className="bg-rose-300 text-white"
-                  >
+                  <Disclosure.Button key={item.name} className="text-teal-900">
                     {item.name}
                   </Disclosure.Button>
                 </Link>
