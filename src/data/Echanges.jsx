70b9018f1763f2037e9a4b4d363fbc7f4ac5abd8
François Chatelier <francois.chatelier@gmail.com>
2022-01-05 11:33:57 +0100

    ajout de la data pour les echanges

--- src/data/Echanges.jsx
+++ src/data/Echanges.jsx
@@ -1,36 +1,102 @@
 const echanges = [
-  {
-    component: "header",
-    data: {
-      title: "Header",
-      burger: { title: "burger" },
-      logo: {
-        src: "./assets/images/logo.png",
-        alt: "logo de l entreprise",
-        cls: "logo",
-        url: "/",
-      },
-    },
-  },
-  {
-    component: "logo",
-    data: {
-      src: "./assets/images/logo.png",
-      alt: "logo de l entreprise",
-      cls: "image",
-    },
-  },
+  // COMPOSANTS
 
-  // COMPOSANT
+  // TITRE
+  {
+    component: "Intitulé",
+    data: {
+      titre: "Dans la trocquerie, il est possible de troquer:",
+      paragraphe: "¡ Uniquement des objets en bon état !",
+    },
+  },
+  // DIFFERENTS PRODUITS
   {
     component: "product",
     data: {
-      title: "Les différents produits",
-      infos:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa sapiente, fugiat ullam quam, dolore at animi excepturi eaque eligendi nostrum quod laboriosam, eius architecto repellat. Eos dicta iure assumenda eveniet?",
-      src: "./assets/images/logo.png",
-      alt: "Image représentant un produit",
-      cls: "product",
+      presentation: [
+        {
+          title: "Les différents produits",
+          infos:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa sapiente, fugiat ullam quam, dolore at animi excepturi eaque eligendi nostrum quod laboriosam, eius architecto repellat. Eos dicta iure assumenda eveniet?",
+          src: "./assets/images/.png",
+          alt: "Image représentant un produit",
+          cls: "product",
+        },
+      ],
+    },
+  },
+
+  // LES TYPES D'OBJETS
+  {
+    component: "objets",
+    data: {
+      categorie: [
+        {
+          appartenance: "Vêtements",
+          cible: "Femme ~ Homme ~ Enfant ~ Accessoire ~ Chaussures ...",
+        },
+        {
+          appartenance: "Maison",
+          cible: "Petit mobilier ~ Vaisselle ~ Décoration ~ Puériculture ... ",
+        },
+        {
+          appartenance: "Culture & loisirs",
+          cible:
+            "Livre ~ Jeu ~ CD/K7 ~ vinyle ~ Jeu vidéo ~ Univers sport ... ",
+        },
+      ],
+    },
+  },
+
+  // CATEGORIES
+
+  {
+    component: "Categorie",
+    data: {
+      texte:
+        "La valeur des objets n'est pas la même pour tout le monde. La valeur du produit dans la Trocquerie est considérée différement de son prix d'achat. L'idée est de privilégier l'éthique et se détacher de la valeur monétaire et aussi sentimentale de ceux-ci.",
+      sousTexte: "Les échanges sont catégorisés par un système de gommettes:",
+      gommettes: [
+        {
+          color: "bleu",
+        },
+        {
+          color: "rose",
+        },
+        {
+          color: "jaune",
+        },
+        {
+          color: "vert",
+        },
+      ],
+      principe: [
+        {
+          color: "bleu = bleu",
+        },
+        {
+          color: "rose = rose",
+        },
+        {
+          color: "jaune = jaune",
+        },
+        {
+          color: "vert = vert",
+        },
+      ],
+      infos: "Il est également possible de combiner des couleurs",
+      more: "(de nombreuses combinaisons possibles à retrouver en boutique)",
+    },
+  },
+
+  // FONCTIONNEMENT
+
+  {
+    component: "article",
+    data: {
+      fonctionnement: "Comment ça marche:",
+      explications:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos quo culpa aspernatur libero sint in enim soluta earum facilis, animi sed, neque nemo eveniet ullam laudantium hic nam pariatur. Temporibus?",
     },
   },
 
