4ab4147c9108426309194baa2611eb8c83d08793
maxime baillon <baillonmaximebaillon@gmail.com>
2022-01-05 11:11:53 +0100

    ajout de couleur, taille emplacement du texte

--- src/components/Abonnement.jsx
+++ src/components/Abonnement.jsx
@@ -2,17 +2,19 @@ import React from "react";
 
 const Abonnement = ({ data }) => {
   return (
-    <div className="abonnement">
-      <h1 className="text-center text-[#275852] text-9xl">{data.title}</h1>
+    <div className="text-vert bg-gris_clair text-center">
+      <h1 className="font-light text-h1 pt-7 underline decoration-double underline-offset-8">
+        {data.title}
+      </h1>
       <br />
       <br />
-      <div className="details border-8 border-indigo-500">
+      <div className=" border-3">
         {data.details.map((detail) => (
-          <p className="text-center text-lg">
+          <p className="mx-5 font-bold text-h2">
             {detail.detail}
-            <p className="text-emerald-800">{detail.description1}</p>
-            <p>{detail.description2}</p>
-            <p>{detail.infoDescription1}</p>
+            <p className="font-regular text-normal">{detail.description1}</p>
+            <p className="font-regular text-normal">{detail.description2}</p>
+            <p className="text-mini">{detail.infoDescription1}</p>
             <br />
           </p>
         ))}
