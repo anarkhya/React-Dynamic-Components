fee14593a0d56ad66fb95e44fd68f78f1c77edbb
gregN <naudgr@gmail.com>
2022-01-05 18:40:55 +0100

    classes dynamiques pour composant ArticleProjet.jsx

--- src/data/Concept.jsx
+++ src/data/Concept.jsx
@@ -2,10 +2,14 @@ const concept = [
   {
     component: "articleProjet",
     data: {
+      classContainerVert:
+        "p-4 gap-3 flex items-center flex-col bg-vert text-blanc",
+      classTitle: "text-h1 font-light",
+      classRoundedFullImg: "w-32 rounded-full",
+      classParagraph: "",
       title: "Porteuse de projet",
       src: "./assets/images/placeholder.png",
-      alt: "portrait",
-      cls: "image",
+      alt: "photo",
       caption: "",
       paragraph:
         "Je m’appelle Agathe Violain, j'ai 30 ans et j’habite à Nantes (vertavienne d'origine). Après plusieurs années en tant que chargée de communication chez Pick Up Production, je suis nouvellement entrepreneuse dans l’ESS afin de monter ce projet de trocquerie. Je chine depuis toujours, j’achète (presque) 100% seconde main et j’ai envie de proposer une nouvelle manière de « consommer » dans cet univers.",
