ac0de2d6e26d5369ef285b5f70a59ecbb9851360
Victor Pagnier <victor.pagnier@gmail.com>
2022-01-05 19:13:02 +0100

    Article pratiquement 100% en data + modif footer et header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -15,7 +15,7 @@ const Header = () => {
           <div className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
             <div className="relative flex items-center justify-between h-36">
               <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
-                <Disclosure.Button className="inline-flex items-center justify-center p-2 rounded-md text-teal-900 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
+                <Disclosure.Button className="inline-flex items-center justify-center p-2 rounded-md text-vert focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
                   <span className="sr-only">Open main menu</span>
                   {open ? (
                     <XIcon className="block h-6 w-6" aria-hidden="true" />
@@ -33,7 +33,7 @@ const Header = () => {
                         <a
                           key={item.name}
                           href={item.to}
-                          className="font-bold text-teal-900 hover:text-rose"
+                          className="font-bold text-vert hover:text-rose"
                         >
                           {item.name}
                         </a>
