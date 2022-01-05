23a104ab5a5fc5c181d5e82ce07c04d06c6b90fd
maxime baillon <baillonmaximebaillon@gmail.com>
2022-01-05 14:22:58 +0100

    ajout du composant utilisateur avec un map, déplacement de la data utilisateur dans la data abonnement

--- src/components/Utilisateur.jsx
+++ src/components/Utilisateur.jsx
@@ -2,19 +2,22 @@ import React from "react";
 
 const Utilisateur = ({ data }) => {
   return (
-    <div className="">
-      <div className="bg-gris_clair">
-        <h1 className="text-h1">{data.titre}</h1>
-        {data.textes.map((texte) => (
-          <h3 className="">
-            {texte.detail1}
-            <h3>{texte.detail1}</h3>
-            <h3>{texte.detail1}</h3>
-            <h3>{texte.detail1}</h3>
-            <h3>{texte.detail1}</h3>
-          </h3>
-        ))}
-      </div>
+    <div className="bg-gris_clair text-vert">
+      <h1 className="text-h1 font-light pl-9 text-center underline decoration-double underline-offset-8">
+        {data.titre}
+      </h1>
+      {data.textes.map((texte) => (
+        <div className="font-regular text-h2 pl-4">
+          <h3>{texte.detail1}</h3>
+          <h3>{texte.detail2}</h3>
+          <h3>{texte.detail3}</h3>
+          <h3>{texte.detail4}</h3>
+          <h3>{texte.detail5}</h3>
+          <h3>{texte.detail6}</h3>
+          <h3>{texte.detail7}</h3>
+          <h3>{texte.detail8}</h3>
+        </div>
+      ))}
     </div>
   );
 };
