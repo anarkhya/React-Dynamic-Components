19d27830c77d54f711f2933c001c1ad3646b7e14
gregN <naudgr@gmail.com>
2022-01-06 14:41:34 +0100

    Affichage composant Article.jsx

--- src/data/Services.jsx
+++ src/data/Services.jsx
@@ -2,9 +2,13 @@ const services = [
   {
     component: "article",
     data: {
+      classBg: "p-4 gap-3 flex items-center flex-col bg-gris_clair text-vert",
+      classTitre: "text-h1 font-light",
+      classButton: "rounded-full bg-vert text-white px-6 py-2 text-sm",
       titre: "Services",
       paragraphe:
         "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
+      button: false,
     },
   },
 ];
