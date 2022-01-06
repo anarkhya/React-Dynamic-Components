b390d313bada358648174fa6800fbf477cdb28a1
gregN <naudgr@gmail.com>
2022-01-06 12:41:36 +0100

    classes dynamiques composant ArticleProjet.jsx
    amélioration

--- src/components/ArticleProjet.jsx
+++ src/components/ArticleProjet.jsx
@@ -3,13 +3,9 @@ import React from "react";
 const ArticleProjet = ({ data }) => {
   return (
     <div className="">
-      <div className={data.classContainerVert}>
+      <div className={data.classContainer}>
         <h1 className={data.classTitle}>{data.title}</h1>
-        <img
-          className={data.classRoundedFullImg}
-          src={data.src}
-          alt={data.alt}
-        />
+        <img className={data.classImg} src={data.src} alt={data.alt} />
         <p className={data.classParagraph}>{data.paragraph}</p>
       </div>
     </div>
