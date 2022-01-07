3cc11cd1bfd4901820e800fcc8c62d9b977a941d
François Chatelier <francois.chatelier@gmail.com>
2022-01-07 16:21:18 +0100

    commentaire sur les composants

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -5,6 +5,7 @@ const Article = ({ data }) => {
     <div className={data.classBg}>
       <h1 className={data.classTitre}>{data.titre}</h1>
       <p>{data.paragraphe}</p>
+      {/* Si il y a du contenu, il s'affiche sinon rien */}
       {data.paragraphe2 ? <p>{data.paragraphe2contenu}</p> : ""}
       {data.paragraphe3 ? <p>{data.paragraphe3contenu}</p> : ""}
     </div>
@@ -13,4 +14,4 @@ const Article = ({ data }) => {
 
 export default Article;
 
-// composant article se retrovant sur la page des Echanges
+// composant article se retrouvant sur la page des Echanges
