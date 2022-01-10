4eadeb014920c90f042fd72d78300a0934aa0c2d
gregN <naudgr@gmail.com>
2022-01-10 16:27:38 +0100

    +iframe map, +dynamisation classes & data

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -3,10 +3,14 @@ import React from "react";
 const Map = ({ data }) => {
   return (
     <div className="">
-      <div className="pt-8 objet-center hero container max-w-screen-lg mx-auto pb-2">
-        <a href={data.url}>
-          <img src={data.src} alt={data.alt} />
-        </a>
+      <div className="">
+        <iframe
+          className={data.classMap}
+          title="map"
+          src={data.src}
+          allowFullScreen=""
+          loading="lazy"
+        />
       </div>
     </div>
   );
