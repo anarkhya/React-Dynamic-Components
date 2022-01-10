0a7c301f56c4eabbdca0712fdb113ccdaa05a5e5
François Chatelier <francois.chatelier@gmail.com>
2022-01-10 17:03:04 +0100

    avancée du tailwindcss page echanges

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -4,7 +4,9 @@ const Product = ({ data }) => {
   // composant product se trouvant sur la page Echanges
   return (
     <div className="bg-vert">
-      <h1 className="text-center text-h2 text-blanc pt-4 ">{data.title}</h1>
+      <h1 className="text-center uppercase text-h2 text-blanc pt-4 ">
+        {data.title}
+      </h1>
       <section className="flex flex-col">
         {data.presentation.map((pres) => (
           <div className=" p-8 flex justify-center items-center gap-4 flex-col xl:flex-row">
