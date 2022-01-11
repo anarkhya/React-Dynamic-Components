8822a60b4df4bc6de9bce46a569b33d1f6ee91c5
gregN <naudgr@gmail.com>
2022-01-11 20:00:13 +0100

    retour sur un design pleine page

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Map = ({ data }) => {
   return (
     <div className="">
-      <div className="">
+      <div className="bg-gris_clair ">
         <iframe
           className={data.classMap}
           title="map"
