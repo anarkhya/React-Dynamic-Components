8e79585b2bade29b22a67034e11ce1e47db2b0c6
gregN <naudgr@gmail.com>
2022-01-11 18:22:14 +0100

    minifix

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -9,7 +9,7 @@ const Article = ({ data }) => {
       </div>
       <h1 className={data.classTitre}>{data.titre}</h1>
       <p className={data.clsPara}>{data.paragraphe}</p>
-      {/* Si il y a du contenu, il s'affiche sinon rien */}
+      {/*  Si il y a du contenu, il s'affiche sinon rien */}
       {data.paragraphe2 ? <p>{data.paragraphe2contenu}</p> : ""}
       {data.paragraphe3 ? <p>{data.paragraphe3contenu}</p> : ""}
       {data.button ? (
