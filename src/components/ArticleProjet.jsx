aaaaf12cee5f00951a27bd747098b12d459aef3e
gregN <naudgr@gmail.com>
2022-01-11 12:27:35 +0100

    minifix import

--- src/components/ArticleProjet.jsx
+++ src/components/ArticleProjet.jsx
@@ -1,11 +1,14 @@
 import React from "react";
+import ButtonAdmin from "./ButtonAdmin";
 
 const ArticleProjet = ({ data }) => {
   return (
     <div className="">
       <div className={data.classContainer}>
         <div className="text-right w-full">
-          <ButtonAdmin />
+          <div className="text-right w-full">
+            <ButtonAdmin />
+          </div>
         </div>
         <h1 className={data.classTitle}>{data.title}</h1>
         <img className={data.classImg} src={data.src} alt={data.alt} />
