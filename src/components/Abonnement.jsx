23f6bc65bbe43d619562a2bd809598b15c04216e
gregN <naudgr@gmail.com>
2022-01-13 11:54:32 +0100

    fix imports manquants

--- src/components/Abonnement.jsx
+++ src/components/Abonnement.jsx
@@ -1,12 +1,12 @@
 import React from "react";
-import ButtonAdmin from "./modals/ButtonAdmin";
+import ButtonAdminAbonnement from "./modals/ButtonAdminAbonnement";
 
 const Abonnement = ({ data }) => {
   return (
     <div>
       <div className="p-4 md:text-lg gap-3 mx-auto flex items-center flex-col bg-gris_clair text-vert">
         <div className="text-right w-full">
-          <ButtonAdmin />
+          <ButtonAdminAbonnement />
         </div>
         <h1 className="font-light text-h1">{data.title}</h1>
         <br />
