bf689df891cc5243675feb46f27d90c9c9cb464a
maxime baillon <baillonmaximebaillon@gmail.com>
2022-01-27 15:35:28 +0100

    ajout ternaire pour ne plus afficher icon modal

--- src/components/ButtonAdmin.jsx
+++ src/components/ButtonAdmin.jsx
@@ -20,12 +20,18 @@ import HeaderData from "../data/HeaderData";
  * @return {*} un bouton d'administration sur chaque modal appartenant à différents component
  */
 const ButtonAdmin = ({ type, data }) => {
-  const [modal, setModal] = useState(false);
+  const [modal, setModal] = useState("");
+  // const [visible, setVisible] = useState();
+
+  // const toogleVisibility = () => {
+  //   setVisible((prevState) => ({ visible: !prevState.visible }));
+  // };
 
   const toggle = () => {
     setModal("");
   };
   const getModal = () => {
+    console.log(getModal);
     const myModal = {
       article: <ModalArticle isShowing hide={toggle} data={data} />,
       abonnement: <ModalAbonnement isShowing hide={toggle} data={data} />,
@@ -46,21 +52,25 @@ const ButtonAdmin = ({ type, data }) => {
   return (
     <div className="">
       {/* {console.log(modal)} */}
-      <button type="submit" onClick={() => getModal()}>
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          className="h-5 w-5"
-          viewBox="0 0 20 20"
-          fill="#0ff"
-        >
-          <path d="M17.414 2.586a2 2 0 00-2.828 0L7 10.172V13h2.828l7.586-7.586a2 2 0 000-2.828z" />
-          <path
-            fillRule="evenodd"
-            d="M2 6a2 2 0 012-2h4a1 1 0 010 2H4v10h10v-4a1 1 0 112 0v4a2 2 0 01-2 2H4a2 2 0 01-2-2V6z"
-            clipRule="evenodd"
-          />
-        </svg>
-      </button>
+      {modal ? (
+        <button type="submit" onClick={() => getModal()}>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-5 w-5"
+            viewBox="0 0 20 20"
+            fill="#0ff"
+          >
+            <path d="M17.414 2.586a2 2 0 00-2.828 0L7 10.172V13h2.828l7.586-7.586a2 2 0 000-2.828z" />
+            <path
+              fillRule="evenodd"
+              d="M2 6a2 2 0 012-2h4a1 1 0 010 2H4v10h10v-4a1 1 0 112 0v4a2 2 0 01-2 2H4a2 2 0 01-2-2V6z"
+              clipRule="evenodd"
+            />
+          </svg>
+        </button>
+      ) : (
+        ""
+      )}
       {modal}
     </div>
   );
