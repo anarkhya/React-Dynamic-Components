6a80d498a2bd66e2c6536ba1630a0ca290f87123
maxime baillon <baillonmaximebaillon@gmail.com>
2022-01-27 16:02:59 +0100

    modification ternaire sur buttonAdmin

--- src/pages/AdminConnection.jsx
+++ src/pages/AdminConnection.jsx
@@ -26,7 +26,6 @@ const AdminConnection = () => {
                 placeholder="Mot de passe"
               />
             </label>
-
             <button
               className="transition hover:bg-rose hover:text-vert hover:border-rose active:-skew-y-6 active:translate-y-1 shadow-blanc/30 shadow-[10px_10px_0px_0px] active:shadow-blanc/20 bg-vert border-blanc border-2 text-blanc px-6 py-2 mt-7"
               type="button"
