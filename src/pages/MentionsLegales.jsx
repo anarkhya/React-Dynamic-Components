1b095ef11bb0567b09f74d62d6fce98d9741c4e9
maxime baillon <baillonmaximebaillon@gmail.com>
2022-01-28 12:39:36 +0100

    ajout d'un lien et page pour les mentions légales

---
+++ src/pages/MentionsLegales.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const MentionsLegales = () => {
+  return (
+    <>
+      <div className="">
+        <h1>hello</h1>
+      </div>
+    </>
+  );
+};
+
+export default MentionsLegales;
